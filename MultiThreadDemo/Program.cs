9c97d6804259c0a3e1d76a68c0378cf9351e8582
himanshu01031997 <hs010397@gmail.com>
2022-11-14 22:36:18 +0530

    add:multiple employee

---
+++ MultiThreadDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiThreadDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ MultiThreadDemoTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using MultiThreadDemo;
+
+namespace MultiThreadDemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenEmployeeAddedtotest()
+        {
+            List<EmployeeDetails> empdetails = new List<EmployeeDetails>();
+            {
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "himanshu", "7898323", "manager"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "sam", "7898323", "hr"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "bruce", "7898323", "tech"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "joe", "7898323", "account"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "singham", "7898323", "cleaning"));
+
+                EmployeePayrollOperation employeepayroll = new EmployeePayrollOperation();
+                employeepayroll.AddEmployeeToPayroll(empdetails);
+
+            }
+
+
+        }
+    }
+}
